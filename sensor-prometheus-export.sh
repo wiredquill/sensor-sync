7830e7ceccb6c26b2a7ba25d317fe43683c46fc1
wiredquill <erin@fascist.org>
2018-11-10 07:19:28 -0800

    Update sensor-prometheus-export.sh

--- sensor-prometheus-export.sh
+++ sensor-prometheus-export.sh
@@ -3,7 +3,13 @@
 # This generates a file every 1 minutes
 
 while true; do
+# This will read the Sense Hat sensors and wirte them to the LED and a file called /tmp/sensors.prom
 python3 sensehat-prometheus.py
+
+# Copy the sensor data to node exporter so prometheus can pick it up
 cp /tmp/sensors.prom /var/lib/node_exporter/textfile/
+
+# Sleep for 60 seconds
 sleep 60
+
 done
