0605be79d4db0ef18eb62e7636710490877499aa
wiredquill <erin@fascist.org>
2018-11-11 08:44:29 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-sudo echo 'Acquire::http { proxy "http://raspi-cache:3142"; };' > /etc/apt/apt.conf.d/02proxy 
+sudo -s
 
-sudo apt-get update
-sudo apr-get upgrade
-sudo apt-get install sense-hat
+echo 'Acquire::http { proxy "http://raspi-cache:3142"; };' > /etc/apt/apt.conf.d/02proxy 
+
+apt-get update
+apt-get upgrade
+apt-get install sense-hat
 
 curl -SL https://github.com/prometheus/node_exporter/releases/download/v0.17.0-rc.0/node_exporter-0.17.0-rc.0.linux-armv7.tar.gz > node_exporter.tar.gz && \
-sudo tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
+tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
 
-sudo mkdir -p /var/lib/node_exporter/textfile
-sudo chmod 777 /var/lib/node_exporter/textfile 
+mkdir -p /var/lib/node_exporter/textfile
+chmod 777 /var/lib/node_exporter/textfile 
+
+exit
