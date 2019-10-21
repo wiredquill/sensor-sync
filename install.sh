d40011c7c4b7ecb13168a3d37687799bd310f61e
WiredQuill <wiredquill@gmail.com>
2019-10-21 11:30:13 -0700

    added comments

--- install.sh
+++ install.sh
@@ -13,19 +13,26 @@ sudo tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
 sudo mkdir -p /var/lib/node_exporter/textfile
 sudo chmod 777 /var/lib/node_exporter/textfile 
 
+echo copying to /etc/systemsd/system
 sudo cp nodeexporter.service /etc/systemd/system
 sudo cp nodeexporter-sensor.service /etc/systemd/system
+
+echo copying to /usr/local/bin
 sudo cp sensor-prometheus-export.sh /usr/local/bin
 sudo cp sensehat-prometheus.py /usr/local/bin
 
+echo chmoding
 sudo chmod +x /etc/systemd/system/nodeexporter.service
 sudo chmod +x /etc/systemd/system/nodeexporter-sensor.service
-
-sudo cp sensor-prometheus-export.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/sensor-prometheus-export.sh
 
+echo loading services
 sudo systemctl daemon-reload 
 sudo systemctl enable --now nodeexporter 
 sudo systemctl enable --now nodeexporter-sensor.service
 
-
+echo 
+echo sudo status nodeexporter 
+sudo status nodeexporter 
+echo sudo systemctl status nodeexporter-sensor.service
+sudo systemctl status nodeexporter-sensor.service
