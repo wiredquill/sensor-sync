4e51a5598c87eff2f26e4c9e9e50bc13b09cf57b
WiredQuill <wiredquill@gmail.com>
2019-10-21 09:27:10 -0700

    updated service for sensor sync

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get --assume-yes upgrade
 apt-get --assume-yes install sense-hat
 
-curl -SL https://github.com/prometheus/node_exporter/releases/download/v0.17.0-rc.0/node_exporter-0.17.0-rc.0.linux-armv7.tar.gz > node_exporter.tar.gz && \
+curl -SL https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-armv7.tar.gz > node_exporter.tar.gz && \
 tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
 
 mkdir -p /var/lib/node_exporter/textfile
