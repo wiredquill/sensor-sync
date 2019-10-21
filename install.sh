c20a8f873853116ea8047b42b3c76c9830a518cf
WiredQuill <wiredquill@gmail.com>
2019-10-21 09:33:10 -0700

    added sudo

--- install.sh
+++ install.sh
@@ -3,20 +3,24 @@
 
 # echo 'Acquire::http { proxy "http://raspi-cache:3142"; };' > /etc/apt/apt.conf.d/02proxy 
 
-apt-get update
-apt-get --assume-yes upgrade
-apt-get --assume-yes install sense-hat
+sudo apt-get update
+sudo apt-get --assume-yes upgrade
+sudo apt-get --assume-yes install sense-hat
 
 curl -SL https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-armv7.tar.gz > node_exporter.tar.gz && \
-tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
+sudo tar -xvf node_exporter.tar.gz -C /usr/local/bin/ --strip-components=1
 
-mkdir -p /var/lib/node_exporter/textfile
+sudo mkdir -p /var/lib/node_exporter/textfile
 chmod 777 /var/lib/node_exporter/textfile 
 
-cp nodeexporter.service /etc/systemd/system
+sudo cp nodeexporter.service /etc/systemd/system
+sudo cp nodeexporter-sensor.service /etc/systemd/system
 
-systemctl daemon-reload 
-systemctl enable nodeexporter 
-systemctl start nodeexporter
+sudo cp sensor-prometheus-export.sh /usr/local/bin
+sudo chmod +x /usr/local/bin/sensor-prometheus-export.sh
+
+sudo systemctl daemon-reload 
+sudo systemctl enable --now nodeexporter 
+sudo systemctl enable --now nodeexporter-sesnor
 
 
